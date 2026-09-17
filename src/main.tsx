37b8c15396a153b01bc081845a5e5c6fb11240fd
onlysky17 <70479008+onlysky17@users.noreply.github.com>
2026-09-17 14:52:47 +0700

    feat: install cart feedback on storefront

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Storefront from './Storefront'
 import Admin from './Admin'
+import { installCartFeedback } from './cart-feedback'
 import './index.css'
 import './mobile-fixes.css'
 import './typography-fixes.css'
 import './admin-polish.css'
 import './product-badges.css'
 import './cart.css'
+import './cart-feedback.css'
 
 const root = document.getElementById('root')!
 const screen = location.pathname.startsWith('/admin') ? <Admin /> : <Storefront />
+
+if (!location.pathname.startsWith('/admin')) installCartFeedback()
 
 ReactDOM.createRoot(root).render(<React.StrictMode>{screen}</React.StrictMode>)
